c4593b33a33e0228bb0c284d852f9e4b836fd155
Laguna <laguna_999@web.de>
2015-04-18 18:41:44 +0200

    adding playermovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+
+    //private Transform _t;
+    public GameObject Rotator;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	   // _t = this.transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+	    if (Input.GetAxis("Horizontal") > 0)
+	    {
+	        MoveRight();
+	    }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            MoveLeft();
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            MoveDown();
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            MoveUp();
+        }
+
 	}
+
+    private void MoveDown()
+    {
+        Rotator.transform.Rotate(new Vector3(1, 0, 0), 2.25f);
+    }
+
+    private void MoveUp()
+    {
+        Rotator.transform.Rotate(new Vector3(1, 0, 0), -2.25f);
+    }
+
+    private void MoveLeft()
+    {
+        Rotator.transform.Rotate(new Vector3(0, 0, 1), 2.25f);
+    }
+
+    private void MoveRight()
+    {
+        Rotator.transform.Rotate(new Vector3(0, 0, 1), -2.25f);
+    }
+
+
 }
