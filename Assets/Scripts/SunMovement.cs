1969e3209f29c84f779d22419c0bafda81519742
Laguna <laguna_999@web.de>
2015-04-18 15:55:41 +0200

    adding scripts

---
+++ Assets/Scripts/SunMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunMovement : MonoBehaviour
+{
+
+    public float Distance = 1.0f;
+    public float Inclination = 10.0f;   // in degree
+    public float RotationFrequency = 0.8f;
+    private float _timeTotal = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float t = Time.fixedDeltaTime;
+        _timeTotal += t;
+
+        float x = Distance * Mathf.Sin(RotationFrequency * _timeTotal / Mathf.PI) *
+                                    Mathf.Cos(Mathf.Deg2Rad * Inclination);
+
+        float z = Distance * Mathf.Cos(RotationFrequency * _timeTotal / Mathf.PI) *
+                                    1.0f;
+        float y = Distance * Mathf.Sin((RotationFrequency * _timeTotal + 0.25f) / Mathf.PI) *
+                                    Mathf.Sin(Mathf.Deg2Rad * Inclination);
+        this.transform.position = new Vector3(x, y, z);
+
+    }
+}
