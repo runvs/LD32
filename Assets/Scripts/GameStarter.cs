797c779c7e8a10ee7137bce65a6dc00cca0cbc3a
Laguna <laguna_999@web.de>
2015-04-19 18:58:18 +0200

    adding menu scene

---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStarter : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void ChangeLevel()
+    {
+        Application.LoadLevel("playermove");
+    }
+
+    public void StartGame()
+    {
+        Invoke("ChangeLevel", 0.15f);
+
+    }
+}
