eda9c8ed14a2b67e1353850855376bcf54130f98
Laguna <laguna_999@web.de>
2015-04-19 13:02:28 +0200

    changed values for input manager, so axis do not feel that wobbly

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerShooter : MonoBehaviour {
+public class PlayerShooter : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject ShotTemplate;
+    private float _inputTimer = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_inputTimer > 0)
+        {
+            _inputTimer -= Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                Fire();
+            }
+        }
+
+    }
+
+    private void Fire()
+    {
+        Vector3 pos = this.transform.position;
+        pos *= 0.85f;
+        GameObject i = Instantiate(ShotTemplate, pos, Quaternion.Euler(0, 0, 0)) as GameObject;
+        _inputTimer += 0.75f;
+    }
 }
